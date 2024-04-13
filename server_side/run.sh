0b9bc289567786970cb567c7495e68456c37529b
CookedMelon <cc2414635846@163.com>
2024-04-13 16:05:18 +0800

    update readme & title

--- server_side/run.sh
+++ server_side/run.sh
@@ -5,4 +5,5 @@ export VISFUZZ_BUILD=$PWD
 cd $DEMO_PATH/re2
 cd repo
 nohup $TOOL_PATH/fuzz/afl/afl-fuzz -i in -o out ./app @@ &
+pip install -r $TOOL_PATH/requirements.txt
 python $TOOL_PATH/open_file_server.py 6767
